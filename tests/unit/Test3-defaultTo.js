c72408e545244142bc6ddd1173a8d9baa66bdd13
Pauli <pauli.leppanen@tuni.fi>
2023-12-04 07:07:39 +0200

    Added tests for defaultTo.js

---
+++ tests/unit/Test3-defaultTo.js
@@ -0,0 +1,35 @@
+import chai from 'chai';
+import defaultTo from '../../src/defaultTo.js'; // FUT SUT DUT Source file
+const { expect } = chai;
+
+// Tests
+
+describe('Test 3. defaultTo /src/defaultTo.js function', () => {
+  it('should return defaultValue when value is `null`', () => {
+    expect(defaultTo(null, 1)).to.equal(1);
+  });
+
+  it('should return defaultValue when value is `Nan`', () => {
+    expect(defaultTo(NaN, 1)).to.equal(1);
+  });
+
+  it('should return defaultValue when value is `undefined`', () => {
+    expect(defaultTo(undefined, 1)).to.equal(1);
+  });
+
+  it('should return value even if it is falsy', () => {
+    // List of falsy values https://developer.mozilla.org/en-US/docs/Glossary/Falsy
+    expect(defaultTo(false, 1)).to.equal(false);
+    expect(defaultTo("", 1)).to.equal("");
+    expect(defaultTo(0, 1)).to.equal(0);
+  });
+
+  if('should return value when it is valid', () => {
+    expect(defaultTo(1,2).to.equal(1));
+    expect(defaultTo('test',0).to.equal('test'));
+    expect(defaultTo([1,2,3],10).to.deep.equal([1,2,3]));
+    expect(defaultTo({test:'test'})).to.deep.equal({test:'test'});
+    expect(defaultTo(true,false)).to.equal(true);
+  });
+
+});
